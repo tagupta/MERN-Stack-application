d98e3b8c996f85def5898011c09135e8836622bb
tagupta <guptatanukhandelwal.96@gmail.com>
2022-07-05 03:08:18 +0530

    feat: heroku post build updates

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
-
+const path = require('path');
 const items = require('./routes/api/items')
-
-const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
@@ -14,5 +12,14 @@ const app = express();
 //Bodyparser middleware
 app.use(bodyParser.json());
 app.use('/api/items', items);
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set Static folder
+    app.use(express.static('client_side/build'));
 
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client_side', 'build', 'index.html'));
+    })
+}
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
