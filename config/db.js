687ff728cc8745941fc59cd912fa2d5cc0886e9e
tagupta <guptatanukhandelwal.96@gmail.com>
2022-07-05 16:50:01 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: "../config.env" });
 const connectDB = async () =>{
   try {
-    console.log(`MONGO_URI "${process.env.MONGO_URI}"`);
+    const URI = process.env.MONGO_URI;
+    console.log(`MONGO_URI ${URI}`);
     const conn = await mongoose.connect(`${process.env.MONGO_URI}`,{
       useNewUrlParser: true,
       useUnifiedTopology: true});
