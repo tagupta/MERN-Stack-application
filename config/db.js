4f540855f1294f922975ccafcd2fcf5c164b598e
tagupta <guptatanukhandelwal.96@gmail.com>
2022-07-05 16:55:40 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
   try {
     const URI = process.env.MONGO_URI;
     console.log(`MONGO_URI ${URI}`);
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}`,{
+    const conn = await mongoose.connect("mongodb+srv://tanu:tanu@shoppinglist.kvnb6ek.mongodb.net/?retryWrites=true&w=majority",{
       useNewUrlParser: true,
       useUnifiedTopology: true});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
